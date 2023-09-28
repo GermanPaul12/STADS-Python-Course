fc2d097a2b631eef46768a83e8df747b0d472d8e
GermanPaul12 <german.paul@outlook.de>
2023-09-28 15:01:56 +0200

    add new fuiles and projects

--- Project 9 Web-App Streamlit/Schedule Plan Python Course/⏱️_Schedule_Planner.py
+++ Project 9 Web-App Streamlit/Schedule Plan Python Course/⏱️_Schedule_Planner.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import datetime as dt
 from pathlib import Path
+import os
 
 st.set_page_config(page_title='Schedule Planner',page_icon='⏱️')
 st.title("Schedule Planner")
@@ -14,7 +15,7 @@ st.title("Schedule Planner")
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
 
-content = [read_markdown_file(f"/Users/german/Documents/STADS/STADS-Python-Course/Project 9 Web-App Streamlit/Schedule Plan Python Course/assets/week_{i}.md") for i in range(1,9)]
+content = [read_markdown_file(f"{os.getcwd()}/assets/week_{i}.md") for i in range(1,9)]
 
 
 time_comparison = dt.datetime.strptime("2023-09-21", "%Y-%m-%d")
