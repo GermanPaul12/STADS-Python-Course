1779e002dbfb849c3f243f4bc9b97e2a2add8f52
GermanPaul12 <german.paul@outlook.de>
2023-09-28 16:55:13 +0200

    update file

--- Streamlit/pages/✔️_Ressources.py
+++ Streamlit/pages/✔️_Ressources.py
@@ -4,18 +4,25 @@ from pathlib import Path
 st.set_page_config(page_title='Ressources for the Python course 🐍',page_icon='✔️')
 st.write("----------")
 
+st.title("Ressources for the Python course 🐍")
+
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
 
 with st.container():
+    st.header("Links and Recommendations")
     st.markdown(read_markdown_file("General/links.md"), unsafe_allow_html=True)    
     st.write("----")    
 
 with st.container():
     st.header("Installation Tutorial")
     st.markdown(read_markdown_file("General/Software/installation_tutorial.md"), unsafe_allow_html=True) 
-    st.header("Installation video for Windows")
+    st.write("----")
+    
+with st.container():
+    st.header("Installation videos")
+    st.subheader("Installation video for Windows")
     st.video("https://www.youtube.com/watch?v=tDLO9hE0k-g&t=24s")
-    st.header("Installation video for Mac")
+    st.subheader("Installation video for Mac")
     st.video("https://www.youtube.com/watch?v=bSMtrJlDZc4&t=106s")
-    st.write("----")       
+    st.write("----")           
